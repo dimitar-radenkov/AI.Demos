d37544bd4c38fd95f2c3c2f472d94868839d9a3a
dimitar-radenkov <dimitar.radenkov1@gmail.com>
2025-10-11 06:57:54 +0300

    Fix typo in generic type parameter name in OperationResult

--- AI.Shared/Infrastructure/OperationResult.cs
+++ AI.Shared/Infrastructure/OperationResult.cs
@@ -1,29 +1,29 @@
 ﻿namespace AI.Shared.Infrastructure;
 
-public abstract record OperationResult<TSeft> where TSeft : OperationResult<TSeft>, new()
+public abstract record OperationResult<TSelf> where TSelf : OperationResult<TSelf>, new()
 {
     public bool IsSuccess { get; init; }
     public string? ErrorMessage { get; init; }
 
-    public static TSeft Success() => new() { IsSuccess = true };
-    public static TSeft Failure(string errorMessage) => new()
+    public static TSelf Success() => new() { IsSuccess = true };
+    public static TSelf Failure(string errorMessage) => new()
     {
         IsSuccess = false,
         ErrorMessage = errorMessage
     };
 }
 
-public abstract record OperationResult<TData, TSeft> : OperationResult<TSeft>
-    where TSeft : OperationResult<TData, TSeft>, new()
+public abstract record OperationResult<TData, TSelf> : OperationResult<TSelf>
+    where TSelf : OperationResult<TData, TSelf>, new()
 {
     public TData? Data { get; init; }
 
-    public static TSeft Success(TData data) => new()
+    public static TSelf Success(TData data) => new()
     {
         IsSuccess = true,
         Data = data
     };
-    public new static TSeft Failure(string errorMessage) => new()
+    public new static TSelf Failure(string errorMessage) => new()
     {
         IsSuccess = false,
         ErrorMessage = errorMessage
